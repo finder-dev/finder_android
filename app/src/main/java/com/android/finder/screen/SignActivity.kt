dba32c3e427dec3570347ff160494065e0ae5eaf
whwogjs96 <68106399+whwogjs96@users.noreply.github.com>
2022-06-16 23:19:58 +0900

    jetpack compose 제거
    jetpack compose 제거 및 sign 화면 세팅

--- app/src/main/java/com/android/finder/screen/SignActivity.kt
+++ app/src/main/java/com/android/finder/screen/SignActivity.kt
@@ -1,31 +1,15 @@
 package com.android.finder.screen
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.navigation.compose.rememberNavController
-import com.android.finder.navigation.setUpNavController
-import com.android.finder.ui.theme.FinderTheme
-import com.android.finder.ui.theme.MainColor
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.android.finder.R
+import com.android.finder.databinding.ActivitySignBinding
 
-class SignActivity : ComponentActivity() {
+class SignActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContent {
-            FinderTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Transparent
-                ) {
-                    setUpNavController(controller = rememberNavController())
-                }
-            }
-        }
+        val binding = DataBindingUtil.setContentView<ActivitySignBinding>(this, R.layout.activity_sign)
     }
 }
